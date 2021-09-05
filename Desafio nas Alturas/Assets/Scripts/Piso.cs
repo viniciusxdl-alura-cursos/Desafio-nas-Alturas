b96c5aefa7b1acb243e26bd3d6d9a9cb178c2296
Vinícius Lima <viniciusxdl@outlook.com>
2021-09-04 23:06:43 -0300

    Adicionar piso infinito

---
+++ Desafio nas Alturas/Assets/Scripts/Piso.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Piso : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade;
+
+    private Vector3 posicaoInicial;
+
+    private float tamanhoRealDaImagem;
+
+    private void Awake()
+    {
+        posicaoInicial = transform.position;
+
+        float tamanhoDaImagem = GetComponent<SpriteRenderer>().size.x;
+        float escala = transform.localScale.x;
+
+        tamanhoRealDaImagem = tamanhoDaImagem * escala;
+    }
+
+    private void Update()
+    {
+        float deslocamento = Mathf.Repeat(velocidade * Time.time, tamanhoRealDaImagem);
+
+        transform.position = posicaoInicial + Vector3.left * deslocamento;
+    }
+}
