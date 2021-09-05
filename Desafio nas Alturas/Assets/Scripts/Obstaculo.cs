bfad1ac8bb4047ad0b78faf3d3b98001e736130c
Vinícius Lima <viniciusxdl@outlook.com>
2021-09-04 23:28:07 -0300

    Adicionar variação de posição no eixo Y para os obstáculos

--- Desafio nas Alturas/Assets/Scripts/Obstaculo.cs
+++ Desafio nas Alturas/Assets/Scripts/Obstaculo.cs
@@ -5,6 +5,14 @@ public class Obstaculo : MonoBehaviour
     [SerializeField]
     private float velocidade = 0.5f;
 
+    [SerializeField]
+    private float variacaoDaPosicaoY;
+
+    private void Awake()
+    {
+        transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
+    }
+
     private void Update()
     {
         transform.Translate(Vector3.left * velocidade * Time.deltaTime);
