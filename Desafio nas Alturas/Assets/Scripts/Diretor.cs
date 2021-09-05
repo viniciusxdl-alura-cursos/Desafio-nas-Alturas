4cdd822a775d41ef2607b922012655cb502f3252
Vinícius Lima <viniciusxdl@outlook.com>
2021-09-05 00:09:37 -0300

    Adicionar mensagem de finalização do jogo (Game Over)

--- Desafio nas Alturas/Assets/Scripts/Diretor.cs
+++ Desafio nas Alturas/Assets/Scripts/Diretor.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject imagemGameOver;
+
     public void FinalizarJogo()
     {
         Time.timeScale = 0;
+
+        imagemGameOver.SetActive(true);
     }
 }
